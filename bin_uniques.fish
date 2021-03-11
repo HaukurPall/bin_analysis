5c5ae74dcbdf1047bd72e69c5d996f8bc8eb5b37
Haukur <haukurpalljonsson@gmail.com>
2021-03-11 12:49:18 +0000

    Greiningarvinnu lokið.

---
+++ bin_uniques.fish
@@ -0,0 +1,6 @@
+set BIN_DATA "/home/haukurpj/Resources/Data/DIM/DIM_2020.06_SHsnid.csv"
+# 3=gen, 4=fl, 6=mark
+cut $BIN_DATA -d ";" -f 3,4,6 | sort | uniq > bin_unique_categories.txt
+cut bin_unique_categories.txt -d ";" -f 1 | sort | uniq > unique_gen.txt
+cut bin_unique_categories.txt -d ";" -f 2 | sort | uniq > unique_fl.txt
+cut bin_unique_categories.txt -d ";" -f 3 | sort | uniq > unique_mark.txt
---
+++ run.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from pathlib import Path
+
+from tqdm import tqdm
+
+BIN_DATA = Path("/home/haukurpj/Resources/Data/DIM/DIM_2020.06_SHsnid.csv")
+
+unique_lemmas = {}
+non_unique_lemmas = defaultdict(list)
+with open(BIN_DATA) as f:
+    for line in tqdm(f):
+        form, id, gen, fl, lemma, pos = line.strip().split(";")
+        key = f"{form}-{pos}-{gen}"
+        if key in unique_lemmas:
+            if unique_lemmas[key] != lemma:
+                non_unique_lemmas[form].append(
+                    f"{gen}-{pos}={lemma} or {unique_lemmas[key]}"
+                )
+                # print(f"key={key}, lemma={lemma}, other_lemma={unique_lemmas[key]}")
+        else:
+            unique_lemmas[key] = lemma
+
+for form, key_lemmas in non_unique_lemmas.items():
+    print(f"form={form}, ambiguous={','.join(key_lemmas)}")
