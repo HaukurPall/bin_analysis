5bd8e609e7bced37021abaf161872e31f8707b3a
Haukur <haukurpalljonsson@gmail.com>
2021-03-23 13:26:02 +0000

    Endurnefna skjöl

--- bin_uniques.sh
+++ bin_uniques.sh
@@ -1,4 +1,5 @@
-set BIN_DATA "/home/haukurpj/Resources/Data/DIM/DIM_2020.06_SHsnid.csv"
+#!/bin/bash
+BIN_DATA="/home/haukurpj/Resources/Data/DIM/DIM_2020.06_SHsnid.csv"
 # 3=gen, 4=fl, 6=mark
 cut $BIN_DATA -d ";" -f 3,4,6 | sort | uniq > bin_unique_categories.txt
 cut bin_unique_categories.txt -d ";" -f 1 | sort | uniq > unique_gen.txt
